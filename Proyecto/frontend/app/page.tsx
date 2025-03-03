403347f8e0c91bc9c2f4b0450b257ff6dcaa7711
Sergio Rey <sergio@tally.legal>
2025-03-03 18:40:53 -0500

    fix: retrieve user email and name for Chat component

--- Proyecto/frontend/app/page.tsx
+++ Proyecto/frontend/app/page.tsx
@@ -1,13 +1,19 @@
 import { AsideLeft, AsideRight } from "@/components/aside";
 import { Chat } from "@/components/chat";
 import Footer from "@/components/footer";
+import { auth } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
+  const email = session?.user?.email;
+  const name = session?.user?.name;
+
   return (
     <div className="h-screen flex">
       <AsideLeft />
       <div className="h-full flex-grow flex-shrink">
-        <Chat email={undefined} name={undefined} />
+        <Chat email={email || undefined} name={name || undefined} />
         <Footer />
       </div>
       <AsideRight />
